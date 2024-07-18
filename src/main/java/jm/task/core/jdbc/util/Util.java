4fe4cfb9ceaa8481c0887aadf8c8102e58567630
Yuri <yuriidenisov@gmail.com>
2024-07-18 18:14:15 +0500

    getConnection with sql exceptions

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -3,15 +3,22 @@ package jm.task.core.jdbc.util;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Util {
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection connection;
+
+
+    public static Connection getConnection() {
         String userName = "root";
         String password = "root1991";
         String connectionURL = "jdbc:mysql://localhost:3306/testdb";
 
-        return DriverManager.getConnection(connectionURL, userName, password);
+        try {
+             connection = DriverManager.getConnection(connectionURL, userName, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
     }
 }
